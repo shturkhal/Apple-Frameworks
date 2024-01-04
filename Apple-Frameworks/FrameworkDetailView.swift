23cc9dee34d4ace95e0d5e6596038d415b65a2c3
shturkhal <107053994+shturkhal@users.noreply.github.com>
2024-01-04 14:10:57 +0200

    add new file

---
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks
+//
+//  Created by Іван Штурхаль on 04.01.2024.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
